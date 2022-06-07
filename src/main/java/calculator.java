d25218b050f99a5619713c197f2a4f091cfbcda9
SteelingCow437 <davidandmatt2@yahoo.com>
2022-06-07 09:51:51 -0400

    claculater

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class calculator
 {
     public static void main(String[] args) {
         boolean equationComplete = false;
+        double combination;
         Scanner number = new Scanner(System.in);
         System.out.println("Enter a number:");
         double numberA = number.nextDouble();
         while(!equationComplete) {
-            System.out.println("What would you like to do with that number? (add, subtract, multiply, or divide)");
+            System.out.println("What would you like to do with that number? (add, subtract, multiply, divide, square root)");
             Scanner operator = new Scanner(System.in);
             String operation = operator.nextLine();
+            if (operation.equalsIgnoreCase( "square root"))
+            {
+                combination = Math.sqrt(numberA);
+                System.out.println(combination);
+                equationComplete = true;
+            }
+            else{
             System.out.println("What number would you like to " + operation + " " + numberA + " with/to?");
             Scanner second = new Scanner(System.in);
             double numberB = second.nextDouble();
-            double combination;
             if (operation.equalsIgnoreCase("add")) {
                 combination = numberA + numberB;
                 System.out.println(numberA + " + " + numberB + " = " + combination);
@@ -30,9 +38,13 @@ public class calculator
                 combination = numberA / numberB;
                 System.out.println(numberA + " / " + numberB + " = " + combination);
                 equationComplete = true;
-            } else {
+            }
+            else {
                 System.out.println("invalid operation. Please enter a valid operation.");
             }
+            }
+
+
         }
 
 
